6d84c212a7af63d8e0a314c63e73775a38d737e0
Егор <egorkrivorotov@mail.ru>
2021-06-25 03:34:09 +0300

    Доработки 2-го задания

--- js/show-full-image.js
+++ js/show-full-image.js
@@ -1,55 +1,62 @@
-import {photos} from './show-images.js';
+const fullScreenImage = document.querySelector('.big-picture');
+const photoComments = fullScreenImage.querySelector('.social__comments');
 
-const fullScreenPic = document.querySelector('.big-picture');
-const photoComments = fullScreenPic.querySelector('.social__comments');
-
-const getPictureComments = (el) => {
+const getPictureComments = (comments) => {
   const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < photos[el].comments.length; i++) {
+  for (let i = 0; i < comments.length; i++) {
     const newComment = document.createElement('li');
     newComment.classList.add('social__comment');
-    newComment.innerHTML = `<img class="social__picture" src="${photos[el].comments[i].avatar}" alt="${photos[el].comments[i].name}" width="35" height="35"><p class="social__text">${photos[el].comments[i].message}</p>`;
+    newComment.innerHTML = `<img class="social__picture" src="${comments[i].avatar}" alt="${comments[i].name}" width="35" height="35"><p class="social__text">${comments[i].message}</p>`;
     fragment.appendChild(newComment);
   }
 
-  return photoComments.appendChild(fragment);
+  return fragment;
 };
 
-const pictures = document.querySelectorAll('.picture');
+const bigImage = fullScreenImage.querySelector('.big-picture__img');
+const likesCount = fullScreenImage.querySelector('.likes-count');
+const allCommentsNumber = fullScreenImage.querySelector('.comments-count');
+const photoCaption = fullScreenImage.querySelector('.social__caption');
+const commentsCount = fullScreenImage.querySelector('.social__comment-count');
+const commentsLoader = fullScreenImage.querySelector('.comments-loader');
 
-pictures.forEach((photo) => {
-  photo.addEventListener('click', () => {
-
-    const i = Array.prototype.slice.call(pictures).indexOf(photo); //превращаю коллекцию в массив, так как photo нельзя использователь как счетчик
-
-    fullScreenPic.querySelector('.big-picture__img').children[0].src = photos[i].url;
-    fullScreenPic.querySelector('.likes-count').textContent = photos[i].likes;
-    fullScreenPic.querySelector('.comments-count').textContent = photos[i].comments.length;
-    photoComments.innerHTML = ''; //удаляю захардкоженные комментарии из раметки
-    getPictureComments(i);
-    fullScreenPic.querySelector('.social__caption').textContent = photos[i].description;
-
-    fullScreenPic.classList.remove('hidden');
-
-    fullScreenPic.querySelector('.social__comment-count').classList.add('hidden');
-    fullScreenPic.querySelector('.comments-loader').classList.add('hidden');
-
-    document.body.classList.add('modal-open');
-
-    document.addEventListener('keydown', (evt) => {
-      if (evt.key === 'Escape' || evt.key === 'Esc') {
-        evt.preventDefault();
-        fullScreenPic.classList.add('hidden');
-        document.body.classList.remove('modal-open');
-      }
-    });
-  });
-});
-
-const FullPicCloseButton = fullScreenPic.querySelector('.big-picture__cancel');
-
-FullPicCloseButton.addEventListener('click', () => {
-  fullScreenPic.classList.add('hidden');
+const closeFullImage = () => {
+  fullScreenImage.classList.add('hidden');
   document.body.classList.remove('modal-open');
+};
+
+const onFullImageEscKeydown = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    closeFullImage();
+    document.removeEventListener('keydown', onFullImageEscKeydown);
+  }
+};
+
+const showFullImage = (photo) => {
+  bigImage.children[0].src = photo.url;
+  likesCount.textContent = photo.likes;
+  allCommentsNumber.textContent = photo.comments.length;
+  photoComments.innerHTML = '';
+  photoComments.appendChild(getPictureComments(photo.comments));
+  photoCaption.textContent = photo.description;
+
+  fullScreenImage.classList.remove('hidden');
+
+  commentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onFullImageEscKeydown);
+};
+
+const fullImageCloseButton = fullScreenImage.querySelector('.big-picture__cancel');
+
+fullImageCloseButton.addEventListener('click', () => {
+  closeFullImage();
+  document.addEventListener('keydown', onFullImageEscKeydown);
 });
+
+export {showFullImage};
--- js/show-images.js
+++ js/show-images.js
@@ -1,4 +1,5 @@
 import {getMocks} from './get-mocks.js';
+import {showFullImage} from './show-full-image.js';
 
 const NUMBER_PHOTOS_PER_PAGE = 25;
 const imageContainer = document.querySelector('.pictures');
@@ -8,13 +9,19 @@ const imageTemplate = document.querySelector('#picture')
 
 const getImageFragment = (data, template) => {
   const fragment = document.createDocumentFragment();
-  data.forEach(({url, likes, comments}) => {
+  data.forEach((photo) => {
+    const {url, likes, comments} = photo;
+    const imageSampleHandler = () => {
+      showFullImage(photo);
+    };
     const imageSample = template.cloneNode(true);
     imageSample.querySelector('.picture__img').src = url;
     imageSample.querySelector('.picture__likes').textContent = likes;
     imageSample.querySelector('.picture__comments').textContent = comments.length;
+    imageSample.addEventListener('click', imageSampleHandler);
     fragment.appendChild(imageSample);
   });
+
   return fragment;
 };
 
